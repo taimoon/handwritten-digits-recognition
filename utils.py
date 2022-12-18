a1e287f5a0324f76571e252b2a5e85e2d22763a5
Leong Teng Man <41246170+taimoon@users.noreply.github.com>
2022-12-18 17:51:36 +0800

    update

---
+++ utils.py
@@ -0,0 +1,27 @@
+def display_digits(digits, predictions, labels, title, n):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+    '''
+    Adapated from: https://colab.research.google.com/github/GoogleCloudPlatform/tensorflow-without-a-phd/blob/master/tensorflow-mnist-tutorial/keras_01_mnist.ipynb
+    
+    Author: Martin Gorner
+    '''
+    plt.rc('xtick', top=False, bottom=False, labelsize='large')
+    plt.rc('axes', edgecolor='white')
+    plt.rc('figure', facecolor='F0F0F0', figsize=(16,9))
+    
+    fig = plt.figure(figsize=(13,3))
+    digits = np.reshape(digits, [n, 28, 28])
+    digits = np.swapaxes(digits, 0, 1)
+    digits = np.reshape(digits, [28, 28*n])
+    plt.yticks([])
+    plt.xticks([28*x+14 for x in range(n)], predictions)
+    plt.xticks
+    plt.grid(visible=None)
+    for i,t in enumerate(plt.gca().xaxis.get_ticklabels()):
+        if predictions[i] != labels[i]: t.set_color('red') # bad predictions in red
+    plt.imshow(digits, cmap = 'gray_r')
+    plt.grid(None)
+    plt.title(title)
+    plt.rcParams.update(plt.rcParamsDefault) # reset to default
